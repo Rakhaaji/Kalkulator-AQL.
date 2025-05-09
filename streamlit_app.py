bd16d37a5552efe9933ef5615b314e7a4e96e5b4
Rakhaaji <rakhahendriaji28373@gmail.com>
2025-05-09 20:21:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,6 +104,7 @@ if page == "Beranda":
 # ------------------------
 elif page == "Kalkulator AQL":
     st.title("⚖️ Kalkulator AQL")
+
     
         # Input
     lot_size = st.number_input("Ukuran Lot", min_value=1, value=500)
