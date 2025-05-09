2fd709f4ff845efb25efef3c35d0b0c07952148e
Rakhaaji <rakhahendriaji28373@gmail.com>
2025-05-09 20:03:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if page == "Beranda":
             border-radius: 12px;
         }
         .feature-box {
-            background-color: #e2e8f0;
+            background-color:(to top, #000000, #b7791f, #f6ad55);
+
             padding: 15px;
             border-radius: 10px;
             margin-bottom: 10px;
