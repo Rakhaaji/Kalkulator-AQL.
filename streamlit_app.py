9146ae8764576d2ce95327e3ea89c4c002b8bd51
Rakhaaji <rakhahendriaji28373@gmail.com>
2025-05-09 19:32:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,17 +17,6 @@ def hitung_acceptance(sample_size, aql_percent):
 # ------------------------
 if page == "Beranda":
 
-st.markdown("""
-    <style>
-    .stApp {
-        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
-                    url('https://img.freepik.com/free-photo/person-paying-using-nfc-technology_23-2149893740.jpg');
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        color: white !important;
-    }
-
     # Gaya industri dan latar belakang
     st.markdown("""
         <style>
